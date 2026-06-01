f71a235204cebd48138335174d78cfab826921cf
Erica <seuemail@email.com>
2026-06-01 16:59:03 -0300

    Include Postgres driver for Vercel

--- api/index.js
+++ api/index.js
@@ -2,4 +2,7 @@ if (process.env.NODE_ENV === 'production' && !process.env.DATABASE_URL) {
   process.env.NODE_ENV = 'development';
 }
 
+require('pg');
+require('pg-hstore');
+
 module.exports = require('../app');
